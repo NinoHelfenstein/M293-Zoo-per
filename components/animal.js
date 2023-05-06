276646fd40bb911d1e4f96f832b43367c5d733c2
NinoHelfenstein <nino.helfenstein@hispeed.ch>
2023-05-06 16:20:05 +0200

    created a component for the animal pages

---
+++ components/animal.js
@@ -0,0 +1,27 @@
+class Animal extends HTMLElement {
+  constructor(attributes) {
+      super();
+      if (attributes) {
+          Object.keys(attributes).forEach(attr => {
+              this.setAttribute(attr, attributes[attr]);
+          });
+      }
+  }
+
+  connectedCallback() {
+      this.innerHTML = `
+        <div class="animal">
+          <img src="${this.getAttribute('image')}">
+          <div class="name-age-wrapper">
+            <div><b>${this.getAttribute('name')}</b></div>
+            <div>${this.getAttribute('age')}</div>
+          </div>
+          <div class="desc">
+            ${this.getAttribute('description')}
+          </div>
+        </div>
+    `;
+  }
+}
+
+customElements.define("zoo-per-animal", Animal);
