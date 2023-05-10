2d4c0ae70405d7dfbb3b561ce039c059c90c7d96
NinoHelfenstein <nino.helfenstein@hispeed.ch>
2023-05-10 15:19:32 +0200

    img link fix

--- components/nav.js
+++ components/nav.js
@@ -11,18 +11,38 @@ class Nav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <nav>
-            <a href="${this.getAttribute("root")}index.html"><img alt="Logo" src="/assets/logo.png" height="70"></a>
+            <a href="${this.getAttribute(
+              "root"
+            )}index.html"><img alt="Logo" src="${this.getAttribute("root")}assets/logo.png" height="70"></a>
             <ul>
-                <li><a href="${this.getAttribute("root")}screens/about/about.html">About</a></li>
-                <li><a href="${this.getAttribute("root")}screens/tickets/tickets.html">Tickets</a></li>
-                <li><a href="${this.getAttribute("root")}screens/animals/animals.html">Animals</a>
+                <li><a href="${this.getAttribute(
+                  "root"
+                )}screens/about/about.html">About</a></li>
+                <li><a href="${this.getAttribute(
+                  "root"
+                )}screens/tickets/tickets.html">Tickets</a></li>
+                <li><a href="${this.getAttribute(
+                  "root"
+                )}screens/animals/animals.html">Animals</a>
                     <ul>
-                        <li><a href="${this.getAttribute("root")}screens/animals/animals.html">View all Animals</a></li>
-                        <li><a href="${this.getAttribute("root")}screens/animals/flog.html">Flog</a></li>
-                        <li><a href="${this.getAttribute("root")}screens/animals/flecko.html">Flecko</a></li>
-                        <li><a href="${this.getAttribute("root")}screens/animals/usain-blob.html">Usain Blob</a></li>
-                        <li><a href="${this.getAttribute("root")}screens/animals/rooster-coleman.html">Rooster Coleman</a></li>
-                        <li><a href="${this.getAttribute("root")}screens/animals/emaeaehnuel-lasker.html">Emäähnuel Lasker</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/animals.html">View all Animals</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/flog.html">Flog</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/flecko.html">Flecko</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/usain-blob.html">Usain Blob</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/rooster-coleman.html">Rooster Coleman</a></li>
+                        <li><a href="${this.getAttribute(
+                          "root"
+                        )}screens/animals/emaeaehnuel-lasker.html">Emäähnuel Lasker</a></li>
                     </ul>
                 </li>
             </ul>
