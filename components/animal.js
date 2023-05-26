8af738f75a061769686357a02a60a75e91557d98
NinoHelfenstein <nino.helfenstein@hispeed.ch>
2023-05-26 10:29:33 +0200

    improved animal styling, thx JannisMilz

--- components/animal.js
+++ components/animal.js
@@ -10,16 +10,19 @@ class Animal extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        <div class="animal">
-          <img src="${this.getAttribute("image")}" alt="${this.getAttribute("alt")}">
-          <div class="name-age-wrapper">
-            <div><b>${this.getAttribute("name")}</b></div>
-            <div>${this.getAttribute("age")}</div>
+      <div class="animal">
+        <img src="../../assets/flog.png" alt="Flog, Flamingo Dog">
+        <div class="name-age-wrapper">
+          <div>
+            <b class="name">Flog</b></div>
+            <div class="age">
+              1.5 Years
+            </div>
           </div>
-          <div class="desc">
-            ${this.getAttribute("description")}
-          </div>
+        <div class="desc">
+          Flog, the extraordinary Flamingo Dog, is a captivating canine companion that effortlessly merges the grace of a flamingo with the loyalty of a dog. With its slender legs, vibrant feathers, and playful nature, Flog stands out as a truly remarkable and enchanting companion. Whether gracefully striding or engaging in playful antics, Flog brings a touch of whimsy and joy to every moment, capturing the hearts of all who encounter its unique charm. Its remarkable appearance and endearing personality make Flog an extraordinary addition to any family seeking a loyal and fascinating four-legged friend.
         </div>
+      </div>
     `;
   }
 }
